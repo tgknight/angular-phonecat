9b72d80ea8c50efff4d3b681b02d19241f588925
Thanapat Bhunnachet <tgcruasde@gmail.com>
2015-08-03 13:56:16 +0700

    Step-4

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
+/* http://docs.angularjs.org/guide/e2e-testing */
 
 describe('PhoneCat App', function() {
 
@@ -21,6 +21,26 @@ describe('PhoneCat App', function() {
 
             query.sendKeys('motorola');
             expect(phoneList.count()).toBe(2);
+        });
+
+        it('should be possible to control phone order via the drop down select box', function() {
+            var phoneNameColumn = element.all(by.repeater('phone in phones').column('phone.name'));
+            var query = element(by.model('query'));
+
+            function getNames() {
+                return phoneNameColumn.map(function(elm) {
+                    return elm.getText();
+                });
+            }
+
+            // take note of the order in the expected array
+            // orderProp = age (by default)
+            query.sendKeys('tablet');
+            expect(getNames()).toEqual(["Motorola XOOM\u2122 with Wi-Fi", "MOTOROLA XOOM\u2122"]);
+
+            // orderProp = name
+            element(by.model('orderProp')).element(by.css('option[value="name"]')).click();
+            expect(getNames()).toEqual(["MOTOROLA XOOM\u2122", "Motorola XOOM\u2122 with Wi-Fi"]);
         });
     });
 
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,16 +1,24 @@
 'use strict';
 
 /* jasmine specs for controllers go here */
+describe('PhoneCat controllers', function() {
 
-describe('PhoneListCtrl', function() {
-    var $scope;
     beforeEach(module('phonecatApp'));
-    beforeEach(inject(function($rootScope, $controller) {
-        $scope = $rootScope.$new();
-        $controller('PhoneListCtrl', {"$scope": $scope});
-    }));
 
-    it('should create "phones" model with 3 phones', function() {
-        expect($scope.phones.length).toBe(3);
+    describe('PhoneListCtrl', function() {
+        var $scope;
+
+        beforeEach(inject(function($rootScope, $controller) {
+            $scope = $rootScope.$new();
+            $controller('PhoneListCtrl', {"$scope": $scope});
+        }));
+
+        it('should create "phones" model with 3 phones', function() {
+            expect($scope.phones.length).toBe(3);
+        });
+
+        it('should set the default value of orderProp model to be "age"', function() {
+            expect($scope.orderProp).toBe('age');
+        })
     });
 });
