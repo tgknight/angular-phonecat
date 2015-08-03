d079d1fac933ba542e9a39030141662ebb3b3a81
Thanapat Bhunnachet <tgcruasde@gmail.com>
2015-08-03 16:20:31 +0700

    Step-5

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,17 +2,12 @@
 
 /* Controllers */
 angular.module('phonecatApp', [])
-.controller('PhoneListCtrl', function ($scope) {
-    $scope.phones = [
-        {'name': 'Nexus S',
-        'snippet': 'Fast just got faster with Nexus S.',
-        'age': 1},
-        {'name': 'Motorola XOOM™ with Wi-Fi',
-        'snippet': 'The Next, Next Generation tablet.',
-        'age': 2},
-        {'name': 'MOTOROLA XOOM™',
-        'snippet': 'The Next, Next Generation tablet.',
-        'age': 3}
-    ];
+.controller('PhoneListCtrl', ['$scope', '$http', function ($scope, $http) {
+    $http.get('phones/phones.json')
+    .then(function(res) {
+        $scope.phones = res.data;
+    }, function(res) {
+        console.err("Error retrieving phones/phones.json: " + res.status);
+    });
     $scope.orderProp = 'age';
-});
+}]);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -6,19 +6,33 @@ describe('PhoneCat controllers', function() {
     beforeEach(module('phonecatApp'));
 
     describe('PhoneListCtrl', function() {
-        var $scope;
+        var $scope, $httpBackend;
 
-        beforeEach(inject(function($rootScope, $controller) {
+        beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('phones/phones.json')
+                .respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
             $scope = $rootScope.$new();
             $controller('PhoneListCtrl', {"$scope": $scope});
         }));
 
-        it('should create "phones" model with 3 phones', function() {
-            expect($scope.phones.length).toBe(3);
-        });
+        // it('should create "phones" model with 3 phones', function() {
+        //     expect($scope.phones.length).toBe(3);
+        // });
 
         it('should set the default value of orderProp model to be "age"', function() {
             expect($scope.orderProp).toBe('age');
-        })
+        });
+
+        it('should create "phones" model with 2 phones fetched from xhr', function() {
+            // not fetched yet, thus it should be undefined object
+            expect($scope.phones).toBeUndefined();
+
+            // flush the request, and return $http promise
+            $httpBackend.flush();
+
+            // expect to equal trained response
+            expect($scope.phones).toEqual([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+        });
     });
 });
